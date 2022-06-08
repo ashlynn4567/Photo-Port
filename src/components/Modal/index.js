babaa2a5824ff6ff859ebdddf1cb855704e69160
Ashley's PC <ashlynn4567@gmail.com>
2022-06-07 22:24:58 -0500

    created conditionally rendering modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,28 @@
+// imports
+import React from "react";
+
+
+// Modal component
+function Modal({currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
+
+    return (
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">{name}</h3>
+                <img 
+                    alt="current category" 
+                    src={require(`../../assets/large/${category}/${index}.jpg`)}
+                />
+                <p>{description}</p>
+                <button type="button">
+                    Close this modal
+                </button>
+            </div>
+        </div>
+    );
+};
+
+
+// exports
+export default Modal;
